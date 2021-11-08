da907dc0a951bec76979da1ee943c4bd88f46546
scoutreilly <scouty33@gmail.com>
2021-11-07 19:50:57 -0700

    initial build

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import { Route, Redirect } from "react-router-dom";
-import PrivateRoute from './PrivateRoute';
-import styled from 'styled-components';
+import React from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import styled from "styled-components";
 
-import Header from './Header';
-import LambdaHeader from './LambdaHeader';
-import View from './View';
-import Login from './Login';
-import Logout from './Logout';
+import Header from "./Header";
+import LambdaHeader from "./LambdaHeader";
+import View from "./View";
+import Login from "./Login";
+import Logout from "./Logout";
 
 const App = () => {
   return (
     <AppContainer>
-      <LambdaHeader/>
-      <Header/>
+      <LambdaHeader />
+      <Header />
       <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/view">
+            <View />
+          </Route>
+          <Route exact path="/logout">
+            <Logout />
+          </Route>
+        </Switch>
       </RouteContainer>
     </AppContainer>
-  )
-}
+  );
+};
 
 export default App;
 
@@ -31,13 +42,12 @@ export default App;
 //3. Create a PrivateRoute for View component point to '/view.'
 //4. Create a PrivateRoute for Logout component pointing to '/logout.'
 
-
 const AppContainer = styled.div`
   height: 100%;
-`
+`;
 const RouteContainer = styled.div`
   display: flex;
   height: 85%;
   align-items: center;
   flex-direction: column;
-`
+`;
